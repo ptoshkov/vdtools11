8f6584caab57261d4f0b83a03dd66ad9c0f6989f
ptoshkov <55318963+ptoshkov@users.noreply.github.com>
2026-02-23 12:31:21 +0200

    Improved build.bat formatting

--- vdtools11/build.bat
+++ vdtools11/build.bat
@@ -1,13 +1,17 @@
 @echo Activating VS Dev CMD 64-bit
 @call "%ProgramFiles(x86)%\Microsoft Visual Studio\18\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=amd64
 @call :checkStageSuccessful
+
 @echo Compiling VD Tools 11
 @cl /O2 /EHsc /W4 /DUNICODE *.cpp ole32.lib user32.lib shell32.lib dwmapi.lib advapi32.lib /link /out:vdtools11.exe
 @call :checkStageSuccessful
+
 @echo Creating installer
 @"%ProgramFiles(x86)%\NSIS\makensis.exe" vdtools11.nsi
 @call :checkStageSuccessful
+
 @pause
+exit 0
 
 :checkStageSuccessful
 @if %ERRORLEVEL% NEQ 0 (
